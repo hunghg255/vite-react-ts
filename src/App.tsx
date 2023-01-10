e000fc684312163c0ebb3ddc631021576d7dd5d6
hunghg255 <giahunghust@gmail.com>
2023-01-10 10:27:09 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ function App() {
       {process.env.VITE_TEST}
       <Hello />
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
           <img src='/vite.svg' className='logo' alt='Vite logo' />
         </a>
-        <a href='https://reactjs.org' target='_blank'>
+        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
